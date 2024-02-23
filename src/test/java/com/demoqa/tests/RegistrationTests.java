1a747bcdf4512d6d126f4a359ec3d8e4d760b1b4
Natalya Al-Ansi <alansinatalya@gmail.com>
2024-02-24 02:03:45 +0300

    prepared to launch on Jenkins

--- src/test/java/com/demoqa/tests/RegistrationTests.java
+++ src/test/java/com/demoqa/tests/RegistrationTests.java
@@ -2,8 +2,12 @@ package com.demoqa.tests;
 
 import com.demoqa.pages.RegistrationPage;
 import com.demoqa.testdata.TestData;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static io.qameta.allure.Allure.step;
+
+@Tag("registration")
 public class RegistrationTests extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
@@ -12,62 +16,81 @@ public class RegistrationTests extends TestBase {
     @Test
     void successfulRegistrationAllFields() {
 
-        registrationPage.openPage()
-                .checkFormTitle(testData.formTitle)
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setEmail(testData.email)
-                .setGender(testData.gender)
-                .setPhoneNumber(testData.phone)
-                .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
-                .setSubjects(testData.subject)
-                .setHobbies(testData.hobby)
-                .uploadPicture(testData.picturePath)
-                .setCurrentAddress(testData.address)
-                .setState(testData.state)
-                .setCity(testData.city)
-                .submit();
+        step("Open form", () -> {
+            registrationPage.openPage();
+        });
 
-        registrationPage.checkAppearanceModalWindow()
-                .checkTitleModalWindow(testData.modalWindowtTitle)
-                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResult("Student Email", testData.email)
-                .checkResult("Gender", testData.gender)
-                .checkResult("Mobile", testData.phone)
-                .checkResult("Date of Birth", testData.birthDay + " " + testData.birthMonth + "," + testData.birthYear)
-                .checkResult("Subjects", testData.subject)
-                .checkResult("Hobbies", testData.hobby)
-                .checkResult("Picture", testData.pictureName)
-                .checkResult("Address", testData.address)
-                .checkResult("State and City", testData.state + " " + testData.city);
+        step("Fill in all fields on the form", () -> {
+            registrationPage.checkFormTitle(testData.formTitle)
+                    .setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.email)
+                    .setGender(testData.gender)
+                    .setPhoneNumber(testData.phone)
+                    .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
+                    .setSubjects(testData.subject)
+                    .setHobbies(testData.hobby)
+                    .uploadPicture(testData.picturePath)
+                    .setCurrentAddress(testData.address)
+                    .setState(testData.state)
+                    .setCity(testData.city)
+                    .submit();
+        });
+
+        step("Verify results", () -> {
+            registrationPage.checkAppearanceModalWindow()
+                    .checkTitleModalWindow(testData.modalWindowtTitle)
+                    .checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResult("Student Email", testData.email)
+                    .checkResult("Gender", testData.gender)
+                    .checkResult("Mobile", testData.phone)
+                    .checkResult("Date of Birth", testData.birthDay + " " + testData.birthMonth + "," + testData.birthYear)
+                    .checkResult("Subjects", testData.subject)
+                    .checkResult("Hobbies", testData.hobby)
+                    .checkResult("Picture", testData.pictureName)
+                    .checkResult("Address", testData.address)
+                    .checkResult("State and City", testData.state + " " + testData.city);
+        });
     }
 
     @Test
     void successfulRegistrationRequiredFields() {
 
-        registrationPage.openPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setGender(testData.gender)
-                .setPhoneNumber(testData.phone)
-                .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
-                .submit();
+        step("Open form", () -> {
+            registrationPage.openPage();
+        });
 
+        step("Fill in the required fields on the form", () -> {
+            registrationPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setGender(testData.gender)
+                    .setPhoneNumber(testData.phone)
+                    .setDateOfBirth(testData.birthDay, testData.birthMonth, testData.birthYear)
+                    .submit();
+        });
 
-        registrationPage.checkResult("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResult("Gender", testData.gender)
-                .checkResult("Mobile", testData.phone)
-                .checkResult("Date of Birth", testData.birthDay + " " + testData.birthMonth + "," + testData.birthYear);
+        step("Verify results", () -> {
+            registrationPage.checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResult("Gender", testData.gender)
+                    .checkResult("Mobile", testData.phone)
+                    .checkResult("Date of Birth", testData.birthDay + " " + testData.birthMonth + "," + testData.birthYear);
+        });
     }
 
     @Test
     void failedRegistrationBlankFields() {
 
-        registrationPage.openPage()
-                .submit();
+        step("Open form", () -> {
+            registrationPage.openPage();
+        });
 
+        step("Submit form without filling in any fields", () -> {
+            registrationPage.submit();
+        });
 
-        registrationPage.checkAbsenceModalWindow();
+        step("Check that there is no results window", () -> {
+            registrationPage.checkAbsenceModalWindow();
+        });
     }
 
 }
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,7 +1,14 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
@@ -10,5 +17,25 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
         Configuration.timeout = 3000;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
